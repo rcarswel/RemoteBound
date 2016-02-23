570993293e9987ded9a96464d146d62881ff049d
Robert Carswell <rcarswel@westga.edu>
2016-02-23 11:52:55 -0500

    Launching and Binding to the Remote Service

--- app/src/main/java/edu/westga/cs6242/remotebound/RemoteBoundActivity.java
+++ app/src/main/java/edu/westga/cs6242/remotebound/RemoteBoundActivity.java
@@ -1,13 +1,39 @@
 package edu.westga.cs6242.remotebound;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Messenger;
+import android.support.v7.app.AppCompatActivity;
 
 public class RemoteBoundActivity extends AppCompatActivity {
+    Messenger myService = null;
+    boolean isBound;
+    private ServiceConnection myConnection =
+            new ServiceConnection() {
+                public void onServiceConnected(ComponentName className,
+                                               IBinder service) {
+                    myService = new Messenger(service);
+                    isBound = true;
+                }
+
+                public void onServiceDisconnected(ComponentName className) {
+                    myService = null;
+                    isBound = false;
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_bound);
+
+        Intent intent = new Intent(getApplicationContext(),
+                RemoteService.class);
+
+        bindService(intent, myConnection, Context.BIND_AUTO_CREATE);
     }
 }
