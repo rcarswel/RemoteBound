7644e6200da836d3e9c5314338333820bac8ba2f
Robert Carswell <rcarswel@westga.edu>
2016-02-23 12:00:53 -0500

    Sending a Message to the Remote Service and Tested.

--- app/src/main/java/edu/westga/cs6242/remotebound/RemoteBoundActivity.java
+++ app/src/main/java/edu/westga/cs6242/remotebound/RemoteBoundActivity.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class RemoteBoundActivity extends AppCompatActivity {
     Messenger myService = null;
     boolean isBound;
+
     private ServiceConnection myConnection =
             new ServiceConnection() {
                 public void onServiceConnected(ComponentName className,
@@ -35,5 +39,22 @@ public class RemoteBoundActivity extends AppCompatActivity {
                 RemoteService.class);
 
         bindService(intent, myConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    public void sendMessage(View view) {
+        if (!isBound) return;
+
+        Message msg = Message.obtain();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("MyString", "Message Received");
+
+        msg.setData(bundle);
+
+        try {
+            myService.send(msg);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
