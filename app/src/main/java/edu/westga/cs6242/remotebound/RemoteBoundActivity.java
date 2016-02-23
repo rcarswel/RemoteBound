406469cb18f616e5209a895f59b856c9976104ce
Robert Carswell <rcarswel@westga.edu>
2016-02-23 11:06:49 -0500

    Initial commit

---
+++ app/src/main/java/edu/westga/cs6242/remotebound/RemoteBoundActivity.java
@@ -0,0 +1,13 @@
+package edu.westga.cs6242.remotebound;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemoteBoundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_bound);
+    }
+}
